e9cdf5f1eec8b316a36c92ae0627d5717ef9a6c7
Brenno Santos <devblsds@gmail.com>
2020-02-20 22:45:31 +0000

    Configure React Navigation

--- src/index.js
+++ src/index.js
@@ -1,24 +1,10 @@
 import React from 'react';
-import { StyleSheet, View, Text } from 'react-native';
 
+import 'react-native-gesture-handler';
 import './config/ReactotronConfig';
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  text: {
-    fontSize: 24,
-    fontWeight: '400',
-  },
-});
+import Routes from './routes';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Hello world!</Text>
-    </View>
-  );
+  return <Routes />;
 }
